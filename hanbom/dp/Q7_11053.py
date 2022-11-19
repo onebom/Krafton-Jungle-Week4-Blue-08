6952badfbb6131a0588480a9a1e786943df81d38
onebom <bomsister@gmail.com>
2022-11-19 10:56:10 +0900

    [우한봄/성공]-Q7(11053)

---
+++ hanbom/dp/Q7_11053.py
@@ -0,0 +1,10 @@
+import sys
+n=int(input())
+a=list(map(int,input().split()))
+dp=[0 for i in range(n)]
+for i in range(n):
+    for j in range(i):
+        if a[i]>a[j]:
+            dp[i]=max(dp[i], dp[j])
+    dp[i]+=1
+print(max(dp))
